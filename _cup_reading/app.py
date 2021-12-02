e981f59d9bfdc8a1a4fa42f7585ce62986d5b4cb
Oleg Kupavtsev <olegkupavtsev@gmail.com>
2021-12-02 22:19:16 +0300

    cup reading to file

--- _cup_reading/app.py
+++ _cup_reading/app.py
@@ -12,7 +12,7 @@ Append this dicts to list
 Save this list to file for some time interval, ex 1M or 10M or so on...
 OR
 Add it to DataBase!!!
-Excel file should save every second
+Excel file is saving onChange
 """
 
 # def f():
@@ -25,11 +25,11 @@ Excel file should save every second
 #     f()
 
 #
-LIST_ALL_EXCEL_SHEET: list = []
+# LIST_ALL_EXCEL_SHEET: list = []
 
 
 def read_excel():
-    excel_cup = pd.read_excel('ri_cup.xlsx', sheet_name='Sheet1',)
+    excel_cup = pd.read_excel('ri_cup.xlsm', sheet_name='Sheet1',)
                     # header=0, names=['purchase', 'purchase_price','sale_price', 'sale'],)
                     # converters={"purchase": int, "purchase_price": int, "sale_price": int, "sale": int})
     excel_cup.columns = ['purchase','purchase_price','sale_price','sale']
@@ -37,21 +37,25 @@ def read_excel():
 
 
 def df_to_list(read_excel):
+    list_all_excel_sheet: list = []
     for index, rows in read_excel.iterrows():
         # print(index, rows)
         my_list: list = [rows.purchase, rows.purchase_price,
                         rows.sale_price, rows.sale,]
-        LIST_ALL_EXCEL_SHEET.append(my_list)
+        list_all_excel_sheet.append(my_list)
+    
+    return list_all_excel_sheet
+
 
 
 
 # Save to file deals.py
-def save_deals(deals_list=LIST_ALL_EXCEL_SHEET):
-    with open('cup.py', 'w') as f:
-        f.write(str(LIST_ALL_EXCEL_SHEET))
+# def save_deals(deals_list=LIST_ALL_EXCEL_SHEET):
+def save_deals(result):
+    with open('cup.py', 'a') as f:
+        f.write(str(result))
         # print('Data rewritred and saved to cup.py')
 
-# df_to_list(read_excel())
 
 # for el in LIST_ALL_EXCEL_SHEET:
 #     t = time()
@@ -60,13 +64,16 @@ def save_deals(deals_list=LIST_ALL_EXCEL_SHEET):
 # print(time.time(1635417727.911163))
 
 
+
 start_time = time()
-interval = 1
-for i in range(20):
+interval = 5
+for i in range(4):
     sleep(start_time + i*interval - time())
-    df_to_list(read_excel())
+    save_deals(df_to_list(read_excel()))
+
     # for el in LIST_ALL_EXCEL_SHEET:
     #     t = time()
     #     el.append(ctime(t))
-    save_deals()
+
+
 print('Done')
